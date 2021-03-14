31c0e22de5df04cdb1fe93a0204719cd400b98a5
ninevra <ninevra@gmail.com>
2021-03-13 16:30:40 -0800

    Test semaphores

---
+++ test/semaphore-concurrent.ts
@@ -0,0 +1,22 @@
+import test from 'ava';
+import {SharedContext, SemaphoreDownError} from '../source';
+import synchronize from './_synchronize';
+
+test('attempt to down() semaphore concurrently in different processes', async t => {
+	const {context, release, theirs} = await synchronize({
+		context: new SharedContext(t.title),
+		ours: 'down-second',
+		theirs: 'down-first'
+	});
+
+	const semaphore = context.createSemaphore(t.title, 2);
+	// Wait for them
+	await theirs.acquire();
+	// They hold the resources; try to get them
+	await t.throwsAsync(semaphore.down({wait: false}), {instanceOf: SemaphoreDownError});
+	const result = semaphore.down();
+	// Signal them to release
+	release();
+	// Get the resources
+	await t.notThrowsAsync(result);
+});
---
+++ test/semaphore.ts
@@ -0,0 +1,95 @@
+import test from 'ava';
+import {SharedContext, SemaphoreMismatchError, SemaphoreDownError, Semaphore} from '../source';
+import synchronize from './_synchronize';
+
+test('acquire binary semaphore', async t => {
+	const semaphore = new SharedContext(t.title).createSemaphore(t.title, 1);
+
+	const first = semaphore.down();
+	await t.notThrowsAsync(first);
+
+	const error = await t.throwsAsync<SemaphoreDownError>(semaphore.down({wait: false}), {instanceOf: SemaphoreDownError});
+	t.is(error.semaphoreId, semaphore.id);
+
+	const second = semaphore.down();
+	await semaphore.up();
+	await t.notThrowsAsync(second);
+
+	const third = semaphore.down();
+	await semaphore.up();
+	await t.notThrowsAsync(third);
+});
+
+test('can\'t register mismatched initial values', async t => {
+	const context = new SharedContext(t.title);
+	const semaphoreOne = context.createSemaphore(t.title, 1);
+	await semaphoreOne.down();
+	const semaphoreTwo = context.createSemaphore(t.title, 2);
+	await t.throwsAsync<SemaphoreMismatchError>(semaphoreTwo.down(), {instanceOf: SemaphoreMismatchError});
+});
+
+// Tries to down() the semaphore at increasing amounts. Returns the last amount
+// that succeeds. _If_ nothing else is using the semaphore, this is the
+// semaphore's available capacity.
+async function probe(semaphore: Semaphore) {
+	let amount = 0;
+	try {
+		while (true) {
+			await semaphore.down({amount, wait: false});
+			await semaphore.up({amount});
+			amount++;
+		}
+	} catch (error) {
+		if (error instanceof SemaphoreDownError) {
+			return amount - 1;
+		}
+
+		throw error;
+	}
+}
+
+test('acquire counting semaphore', async t => {
+	const semaphore = new SharedContext(test.meta.file).createSemaphore(t.title, 3);
+	const unblocked: number[] = [];
+
+	await semaphore.down({amount: 2}).then(() => unblocked.push(1));
+	t.is(await probe(semaphore), 1);
+	const second = semaphore.down({amount: 2}).then(() => unblocked.push(2)); // eslint-disable-line promise/prefer-await-to-then
+	t.is(await probe(semaphore), 1);
+	const third = semaphore.down({amount: 1}).then(() => unblocked.push(3)); // eslint-disable-line promise/prefer-await-to-then
+	t.is(await probe(semaphore), 1);
+	await semaphore.up();
+	await second;
+	t.is(await probe(semaphore), 0);
+	await semaphore.up();
+	await third;
+	t.deepEqual(unblocked, [1, 2, 3]);
+});
+
+test('increment semaphore before decrementing', async t => {
+	const semaphore = new SharedContext(test.meta.file).createSemaphore(t.title, 0);
+	t.is(await probe(semaphore), 0);
+	await semaphore.up();
+	t.is(await probe(semaphore), 1);
+	await semaphore.down({wait: false});
+});
+
+test('attempt to down() semaphore concurrently in different processes', async t => {
+	const {context, release, theirs} = await synchronize({
+		context: new SharedContext(t.title),
+		ours: 'down-first',
+		theirs: 'down-second'
+	});
+
+	const semaphore = context.createSemaphore(t.title, 2);
+	// Acquire two units
+	await semaphore.down({amount: 2});
+	// Let them try
+	release();
+	// Wait for them
+	await theirs.acquire();
+	// Release one unit
+	await semaphore.up();
+
+	t.pass();
+});
